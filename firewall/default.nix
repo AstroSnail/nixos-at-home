471705a3c5c9e0375aecc3d4cfd6283056c3f699
Ubuntu <ubuntu@vps-04b3828b.vps.ovh.net>
2023-01-26 22:49:48 +0000

    Use existing nftables service, only install config

--- firewall/default.nix
+++ firewall/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
   boot.modprobeConfig.enable = lib.mkForce true;
@@ -8,6 +8,8 @@
   networking.firewall.allowedTCPPorts = [ 22 ];
   networking.firewall.allowedUDPPorts = [ 51820 ];
   networking.firewall.interfaces.ens3.allowedTCPPorts = [ 123 ];
+
+  environment.etc."nftables.conf".source = config.systemd.services.nftables.serviceConfig.ExecStart;
 
   installScript = builtins.readFile ./install.sh;
 }
--- firewall/install.sh
+++ firewall/install.sh
@@ -3,15 +3,17 @@
 command=${command:?"'command' variable missing!"}
 profile=${profile:?"'profile' variable missing!"}
 
-service_file=/etc/systemd/system/nftables.service
-service_link=/etc/systemd/system/multi-user.target.wants/nftables.service
-service_relative=../nftables.service
+nftables_config=/etc/nftables.conf
+nftables_config_backup=/etc/nftables.conf.bak
 modprobe_file_profile=/etc/modprobe.d/nixos.conf
 modprobe_file=/etc/modprobe.d/firewall.conf
 
 run_install () (
-  ln --symbolic --no-target-directory "${profile}${service_file}" "${service_file}"
-  ln --symbolic --no-target-directory "${service_relative}" "${service_link}"
+  # save possibly pre-existing nftables config
+  if [ -e "${nftables_config}" ]
+  then mv "${nftables_config}" "${nftables_config_backup}"
+  fi
+  ln --symbolic --no-target-directory "${profile}${nftables_config}" "${nftables_config}"
   ln --symbolic --no-target-directory "${profile}${modprobe_file_profile}" "${modprobe_file}"
 )
 
@@ -20,8 +22,11 @@ run_remove () (
   # e.g. to remove a partially failed install
   set +o errexit
   rmlink "${modprobe_file}"
-  rmlink "${service_link}"
-  rmlink "${service_file}"
+  rmlink "${nftables_config}"
+  # restore possibly pre-existing nftables config
+  if [ -e "${nftables_config_backup}" ]
+  then mv "${nftables_config_backup}" "${nftables_config}"
+  fi
 )
 
 "run_${command}" "$@"
