0ac38afe1b79ac71f37b985b095c1adbf074ae68
Ubuntu <ubuntu@vps-04b3828b.vps.ovh.net>
2023-01-26 23:25:48 +0000

    Reduce log spam

--- firewall/default.nix
+++ firewall/default.nix
@@ -3,13 +3,16 @@
 {
   boot.modprobeConfig.enable = lib.mkForce true;
   networking.nftables.enable = true;
-
+  # Reduce logspam
+  networking.firewall.logRefusedConnections = false;
   networking.firewall.rejectPackets = true;
+
   networking.firewall.allowedTCPPorts = [ 22 ];
   networking.firewall.allowedUDPPorts = [ 51820 ];
   networking.firewall.interfaces.ens3.allowedTCPPorts = [ 123 ];
 
-  environment.etc."nftables.conf".source = config.systemd.services.nftables.serviceConfig.ExecStart;
+  environment.etc."nftables.conf".source =
+    config.systemd.services.nftables.serviceConfig.ExecStart;
 
   installScript = builtins.readFile ./install.sh;
 }
