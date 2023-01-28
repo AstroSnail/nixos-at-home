cb64e373ea7cd3a7a666ce401f344b75abae6941
Ubuntu <ubuntu@vps-04b3828b.vps.ovh.net>
2023-01-28 00:51:23 +0000

    Add service linkage to firewall

--- firewall/install.sh
+++ firewall/install.sh
@@ -7,18 +7,22 @@ nftables_config=/etc/nftables.conf
 nftables_config_backup=/etc/nftables.conf.bak
 modprobe_file_profile=/etc/modprobe.d/nixos.conf
 modprobe_file=/etc/modprobe.d/firewall.conf
+nftables_service_file=/lib/systemd/system/nftables.service
+nftables_service_link=/etc/systemd/system/sysinit.target.wants/nftables.service
 
 run_install () (
   # save pre-existing nftables config
   mv --no-target-directory "${nftables_config}" "${nftables_config_backup}"
   ln --symbolic --no-target-directory "${profile}${nftables_config}" "${nftables_config}"
   ln --symbolic --no-target-directory "${profile}${modprobe_file_profile}" "${modprobe_file}"
+  ln --symbolic --no-target-directory "${nftables_service_file}" "${nftables_service_link}"
 )
 
 run_remove () (
   # keep trying to remove
   # e.g. to remove a partially failed install
   set +o errexit
+  rmlink "${nftables_service_link}"
   rmlink "${modprobe_file}"
   rmlink "${nftables_config}"
   # restore pre-existing nftables config
