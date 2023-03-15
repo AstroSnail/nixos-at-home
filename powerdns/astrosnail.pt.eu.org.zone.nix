eaaa27db75830e977487b3b6caf64617ee982bc1
AstroSnail <astrosnail@protonmail.com>
2023-03-15 19:29:59 +0000

    Move info, add wildcard rrset

--- powerdns/astrosnail.pt.eu.org.zone.nix
+++ powerdns/astrosnail.pt.eu.org.zone.nix
@@ -18,10 +18,20 @@ in ''
             A      ${config.ips.sea-ipv4}
             AAAA   ${config.ips.sea-ipv6}
 
+  ; nameservers
   ns1       A      ${config.ips.sea-ipv4}
             AAAA   ${config.ips.sea-ipv6}
   ns2       A      ${config.ips.sea-ipv4}
             AAAA   ${config.ips.sea-ipv6}
+
+  ; info
+  @         CAA    128 issue        "letsencrypt.org; accounturi=https://acme-v02.api.letsencrypt.org/acme/acct/1001995317; validationmethods=dns-01"
+            CAA    128 issuewild    "letsencrypt.org; accounturi=https://acme-v02.api.letsencrypt.org/acme/acct/1001995317; validationmethods=dns-01"
+            CAA      0 iodef        "mailto:${config.email}"
+            CAA      0 contactemail "${config.email}"
+            TXT    "keybase-site-verification=HNPj0etgb3YWy5gfHR9xtMucE44Lh5siUnf4UdQY45g"
+  _ens      TXT    "a=0x4650264Dd8Fb4e32A88168E6206e0779D11800c7"
+  _validation-contactemail  TXT  "${config.email}"
 
   ; hosts
   snail     AAAA   ${config.ips.snail-ipv6}
@@ -45,14 +55,8 @@ in ''
   webgl     CNAME  snail
   www       CNAME  snail
 
-  ; info
-  @         CAA    128 issue        "letsencrypt.org; accounturi=https://acme-v02.api.letsencrypt.org/acme/acct/1001995317; validationmethods=dns-01"
-            CAA    128 issuewild    "letsencrypt.org; accounturi=https://acme-v02.api.letsencrypt.org/acme/acct/1001995317; validationmethods=dns-01"
-            CAA      0 iodef        "mailto:${config.email}"
-            CAA      0 contactemail "${config.email}"
-            TXT    "keybase-site-verification=HNPj0etgb3YWy5gfHR9xtMucE44Lh5siUnf4UdQY45g"
-  _ens      TXT    "a=0x4650264Dd8Fb4e32A88168E6206e0779D11800c7"
-  _validation-contactemail  TXT  "${config.email}"
+  ; experimental, consider removing later
+  *         CNAME  sea
 
   ; funny
   so-you-thought-you-could-just-have-a-subdomain-for-yourself-huh.you-wanted-to-show-the-whole-world-that-you-can-just-ask-for-it.well-let-me-teach-you-a-little-something-from-round-these-parts.one-does-not-simply-get-a-subdomain-from.astrosnail.pt.eu.org.  CNAME  juhu.is.not.malic.ee. ;juhu.internet-box.ch.
